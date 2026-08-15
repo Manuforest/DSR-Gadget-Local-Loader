63d01e58ed6407fc090c412ae6f8df4b1a494021
Manuforest <106248212+Manuforest@users.noreply.github.com>
2026-08-15 22:29:57 +0800

    Declare ImGui Win32 message handler

---
+++ DSR-QuickWarp-Native/ImGuiWin32Forward.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Windows.h>
+#include <imgui.h>
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(
+    HWND hWnd,
+    UINT msg,
+    WPARAM wParam,
+    LPARAM lParam);
