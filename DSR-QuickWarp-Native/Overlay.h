fbd978624e4c4e3e2b88333dbd14b2b9096890ab
Manuforest <106248212+Manuforest@users.noreply.github.com>
2026-08-15 22:30:08 +0800

    Forward declare ImGui Win32 handler

--- DSR-QuickWarp-Native/Overlay.h
+++ DSR-QuickWarp-Native/Overlay.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include <Windows.h>
+#include <imgui.h>
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(
+    HWND hWnd,
+    UINT msg,
+    WPARAM wParam,
+    LPARAM lParam);
+
 namespace quickwarp
 {
     void StartOverlay();
