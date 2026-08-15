d4fdd27b2c38d22a321dfe5bd8a3850ae33bb1de
Manuforest <106248212+Manuforest@users.noreply.github.com>
2026-08-15 22:13:30 +0800

    Build PropertyHook separately for QuickWarp

--- DSR-QuickWarp/build.ps1
+++ DSR-QuickWarp/build.ps1
@@ -9,6 +9,7 @@ if (!(Test-Path $propertyHookProject)) {
 
 Push-Location $repoRoot
 try {
+    msbuild 'PropertyHook\PropertyHook\PropertyHook.csproj' /m /p:Configuration=Release /p:Platform=AnyCPU
     msbuild 'DSR-QuickWarp\DSR-QuickWarp.csproj' /m /p:Configuration=Release /p:Platform=x64
 }
 finally {
